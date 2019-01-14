ce07498b283f8fb62aa0f289b40cdbf4b92b551f
Joanne Lee <vernapii@gmail.com>
2019-01-14 12:56:32 -0800

    setup complete

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            SCALE GENERATOR
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
